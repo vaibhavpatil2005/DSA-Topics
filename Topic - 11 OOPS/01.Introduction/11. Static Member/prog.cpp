1829a41858c650404247005b1a92ae5fe871476e
VAIBHAV PATIL <vaibhavpatil843541@gmail.com>
2025-07-30 00:10:15 +0530

    yuy

--- Topic - 11 OOPS/01.Introduction/11. Static Member/prog.cpp
+++ Topic - 11 OOPS/01.Introduction/11. Static Member/prog.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 class Bikes {
     // Static member variable declaration
-    static int numOfBikes; // Belongs to the class, shared by all objects of the class.
+    static int numOfBikes; // Belongs to the class, shared by all objects of the c
     
     int tyreSize;
     int engineSize;
